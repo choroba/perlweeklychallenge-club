378160b382505c75a73050ecadc0dd0e0d916d40
KjetilS <kjetilskotheim@gmail.com>
2026-05-07 10:33:52 +0200

    https://theweeklychallenge.org/blog/perl-weekly-challenge-372/

--- challenge-372/kjetillll/perl/ch-2.pl
+++ challenge-372/kjetillll/perl/ch-2.pl
@@ -1,15 +1,21 @@
 use List::Util 'max';
 
-sub f {
+sub f1 {
     my $max;
     shift =~ qr(
-        (.) .* \1
-        (??{ $max = max( $max, length($&) - 2 ); '^' })
+        (.) (.*) \1
+        (??{ $max = max( $max, length $2 ); '^' })
     )x;
     $max
 }
 
-print f($$_[0]) == $$_[1] ? "ok\n" : "ERROR\n" for
+sub f2 {
+    pop !~ /^(.)(.*\1)?/ ? 0 : max length($2) - 1, f2( $2 . $' )
+}
+
+print f1( $$_[0] ) == $$_[1] ? "ok\n" : "ERROR\n" and
+print f2( $$_[0] ) == $$_[1] ? "ok\n" : "ERROR\n"
+    for
     [ "aaaaa"     =>  3 ],
     [ "abcdeba"   =>  5 ],
     [ "abbc"      =>  0 ],
